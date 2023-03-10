be8e3651687a35b15728a8cd729afff9976f20c0
unknown <yeasin.mohammod018@gmail.com>
2023-03-10 21:22:51 +0600

    some js modified

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('js working')
+var passWord  = document.getElementById('userPassword');
+var showPassToggler  = document.getElementById('showPassToggler');
+
+// Create the function:
+
+showHidePassword = () => {
+    if (passWord.type == 'password') {
+        passWord.setAttribute('type', 'text');
+        showPassToggler.classList.remove('fa-eye-slash');
+        showPassToggler.classList.add('fa-eye');
+    }
+    else {
+        showPassToggler.classList.add('fa-eye-slash');
+        showPassToggler.classList.remove('fa-eye');
+        passWord.setAttribute('type', 'password');
+
+    }
+        
+};
+
+showPassToggler.addEventListener('click', showHidePassword);
